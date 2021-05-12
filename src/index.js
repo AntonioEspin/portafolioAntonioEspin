5dfdf39d43406fd6c7de0c9b6bb97cfc9d474932
AntonioEspin <antoesdev91@gmail.com>
2021-05-12 17:45:30 -0500

    [add] logic icon nav

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const iconImage = document.getElementById('iconImage');
+const navBar = document.getElementById('navBar');
+const headerButton = document.getElementById('headerButton');
+
+iconImage.addEventListener('click', ()=> {
+  const getSrc = iconImage.getAttribute('src');
+  if(getSrc === "../src/static/icon-menu.svg"){
+    headerButton.style.display = 'none'
+    iconImage.setAttribute("src", "../src/static/icon-exit.svg")
+    navBar.classList.add("header__navbarMobile")
+  } else {
+    headerButton.style.display = 'block'
+    iconImage.setAttribute("src", "../src/static/icon-menu.svg")
+    navBar.classList.remove("header__navbarMobile")
+  }
+})
